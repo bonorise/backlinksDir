35e3a0129b259e1a2063a534239ac23ccb60d098
bonorise <bonorise@qq.com>
2025-04-01 00:30:27 +0800

    table-view

--- app/actions.ts
+++ app/actions.ts
@@ -3,9 +3,8 @@
 import { parseMarkdownTable } from '@/lib/markdown';
 import fs from 'fs';
 import path from 'path';
-import { Backlink } from '@/lib/types';
 
-export async function getBacklinks(): Promise<Backlink[]> {
+export async function getBacklinks() {
   const markdownPath = path.join(process.cwd(), 'data', 'backlinks.md');
   const markdownContent = fs.readFileSync(markdownPath, 'utf-8');
   return parseMarkdownTable(markdownContent);
